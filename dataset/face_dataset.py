173ada0a13d5f0af0e82e54c7386db0ea2dbb667
Dao Quang Huy <daoquanghuy.uet.140598@gmail.com>
2020-05-07 17:38:18 +0700

    change face parser

--- dataset/face_dataset.py
+++ dataset/face_dataset.py
@@ -27,6 +27,8 @@ class FaceDataset(BaseDataset):
         self.anchors = read_anchors(anchor_dir)
         self.dataset = self.parser.parse_dataset()
 
+        print(len(self.dataset))
+
         self.n_classes = len(self.parser.face_names)
 
     def __len__(self):
